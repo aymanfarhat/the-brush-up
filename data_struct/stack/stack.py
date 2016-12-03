34a2cfa9982e547ffece633de6dd9fc9f77ba1d1
Ayman <ayman.farhat1@gmail.com>
2016-12-03 17:19:40 +0200

    Sort a stack to a new one using only another stack as buffer

--- data_struct/stack/stack.py
+++ data_struct/stack/stack.py
@@ -29,3 +29,45 @@ class Stack(object):
             return self.head.data
 
         return None
+
+    def __iter__(self):
+        """Iterable over linked list nodes"""
+        current = self.head
+
+        while current:
+            yield current
+            current = current.next_node
+    
+
+    def is_empty(self):
+        return self.head == None
+
+
+    def sort_desc(self):
+        """Write a program to sort a stack in ascending order (with biggest items on top). 
+        You may use at most one additional stack to hold items, but you may not copy the 
+        elements into any other data structure (such as an array). 
+        The stack supports the following operations: push, pop, peek, and isEmpty."""
+
+        sbuffer = self.__class__()
+        origin = self 
+
+        while not origin.is_empty():
+            current = origin.pop()
+
+            greaters = 0
+
+            if sbuffer.head == None or sbuffer.peek() < current:
+                sbuffer.push(current)
+            else:
+                while sbuffer.peek() > current:
+                    origin.push(sbuffer.pop())
+                    greaters += 1
+
+                origin.push(current)
+
+                while greaters > 0:
+                    sbuffer.push(origin.pop())
+                    greaters -= 1
+
+        return sbuffer
--- data_struct/stack/test_stack.py
+++ data_struct/stack/test_stack.py
@@ -24,3 +24,16 @@ def test_peek():
     assert 'mydata2' == mystack.peek()
     assert 'mydata2' == mystack.pop()
     assert 'mydata' == mystack.peek()
+
+def test_sort_desc():
+    mystack = Stack()
+
+    mystack.push(8)
+    mystack.push(2)
+    mystack.push(5)
+    mystack.push(3)
+    mystack.push(1)
+
+    assert [1, 3, 5, 2, 8] == [n.data for n in mystack]
+
+    assert [8, 5, 3, 2, 1] == [n.data for n in mystack.sort_desc()]
